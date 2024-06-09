57a69f9fff443e1b3af69ce63f79ff91e17a68c0
Shafa Hafshah Nadhifah <shafa.21047@mhs.unesa.ac.id>
2024-06-10 03:13:36 +0700

    updateDataCard

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,111 +1,246 @@
 const Data = [
     {
         id: "1",
-        title: "Pantai Ancol",
+        title: "Pantai Ancol, Jakarta Utara",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-pantaiancol.png",
-        rating: "4,5",
+        rating: "4,8",
         category: "Pantai"
     },
     {
         id: "2",
-        title: "Pantai Carita",
+        title: "Pantai Carita, Jakarta Utara",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-pantaicarita.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Pantai"
     },
     {
         id: "3",
-        title: "Museum Nasional",
+        title: "Museum Nasional, Jakarta Pusat",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-museumnasional.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Museum"
     },
     {
         id: "4",
-        title: "Museum Sejarah",
+        title: "Museum Sejarah, Jakarta Pusat",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-museumsejarah.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Museum"
     },
     {
         id: "5",
-        title: "Museum BI",
+        title: "Museum BI, Jakarta Barat",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-museumBI.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Museum"
     },
     {
         id: "6",
-        title: "Pulau Pari",
+        title: "Pulau Pari, Kepulauan Seribu",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-pulaupari.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Pantai"
     },
     {
         id: "7",
-        title: "Kota Tua",
+        title: "Kota Tua, Jakarta Pusat",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-kotatua.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Museum"
     },
     {
         id: "8",
-        title: "Ancol",
+        title: "Ancol, Jakarta Utara",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-ancol.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Hiburan"
     },
     {
         id: "9",
-        title: "Ragunan",
+        title: "Ragunan, Jakarta Selatan",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-ragunan.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Hiburan"
     },
     {
         id: "10",
-        title: "Skyworld",
+        title: "Sky World, Jakarta Timur",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-skyworld.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Hiburan"
     },
     {
         id: "11",
-        title: "Museum Transport",
+        title: "Museum Transport, Jakarta Timur",
         location: "Jakarta, Indonesia",
         price: "Rp.130.000",
         image: "/images/img-museumtransport.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Museum"
     },
     {
         id: "12",
         title: "Dunia Fantasi",
         location: "Jakarta, Indonesia",
-        price: "Rp.130.000",
+        price: "Rp.150.000",
         image: "/images/img-duniafantasi.png",
-        rating: "4,5",
+        rating: "4,8",
         category:"Hiburan"
+    },
+    {
+        id: "13",
+        title: "Planetarium, Jakarta Timur",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-planetarium.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "14",
+        title: "Museum Wayang, Jakarta Barat",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-museumwayang.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "15",
+        title: "Museum Macam, Jakarta Barat",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-museummacan.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "16",
+        title: "Museum Tekstil, Jakarta Barat",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-museumtekstil.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "17",
+        title: "Galeri Sophilia, Jakarta Utara",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-galerisophilia.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "18",
+        title: "Museum Bahari, Jakarta Utara",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-museumbahari.png",
+        rating: "4,8",
+        category:"Museum"
+    },
+    {
+        id: "19",
+        title: "Pulau Pari, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulaupari.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "20",
+        title: "Pulau Tidung, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulautidung.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "21",
+        title: "Pulau Harapan, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulauharapan.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "22",
+        title: "Pulau Bidadari, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulaubidadari.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "23",
+        title: "Pulau Pramuka, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulaupramuka.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "24",
+        title: "Pulau Pelangi, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulaupelangi.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "25",
+        title: "Pulau Sepa, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulausepa.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "26",
+        title: "Pulau Bintang, Kepulauan Seribu",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-pulaubintang.png",
+        rating: "4,8",
+        category:"Pantai"
+    },
+    {
+        id: "27",
+        title: "Museum Katedral, Jakarta Pusat",
+        location: "Jakarta, Indonesia",
+        price: "Rp.150.000",
+        image: "/images/img-museumkatedral.png",
+        rating: "4,8",
+        category:"Museum"
     },
 ];
 
